845c4e6e03db8b59032593eafade6e3fbc8e4372
Michael Kotthaus <michael.kotthaus@gmx.net>
2025-12-27 00:29:44 +0100

    use arrays for LEDs and push-buttons and iterate over elements

--- src/main.rs
+++ src/main.rs
@@ -21,26 +21,25 @@ fn main() -> ! {
     let mut led_yel = LightEmittingDiode { led: pins.d10.into_output().downgrade(), state: false, color: "yellow" };
     let mut led_grn = LightEmittingDiode { led: pins.d11.into_output().downgrade(), state: false, color: "green" };
     let mut led_blu = LightEmittingDiode { led: pins.d3.into_output().downgrade(),  state: false, color: "blue" };
+    let mut leds = [&mut led_red, &mut led_yel, &mut led_grn, &mut led_blu];
 
     let mut push_button_red = PushButton { pbt: pins.a0.into_pull_up_input().downgrade(), was_pressed: false };
     let mut push_button_yel = PushButton { pbt: pins.a1.into_pull_up_input().downgrade(), was_pressed: false };
     let mut push_button_grn = PushButton { pbt: pins.a2.into_pull_up_input().downgrade(), was_pressed: false };
     let mut push_button_blu = PushButton { pbt: pins.a3.into_pull_up_input().downgrade(), was_pressed: false };
+    let mut push_buttons = [&mut push_button_red, &mut push_button_yel, &mut push_button_grn, &mut push_button_blu];
 
     serial_println!("Rust application 'Advent wreath' is running. Enjoy.");
 
     // start-up light show
-    for _n in 0..12 {
-        led_red.generate_pulse();
-        led_yel.generate_pulse();
-        led_grn.generate_pulse();
-        led_blu.generate_pulse();
-        if push_button_red.is_pressed()
-            || push_button_yel.is_pressed()
-            || push_button_grn.is_pressed()
-            || push_button_blu.is_pressed()
-        {
-            break;
+    'outer: for _n in 0..12 {
+        for led in &mut leds {
+            led.generate_pulse();
+        }
+        for pb in &mut push_buttons {
+            if pb.is_pressed() {
+                break 'outer;
+            }
         }
     }
 
@@ -48,22 +47,19 @@ fn main() -> ! {
     serial_println!("(John 8:12)");
 
     loop {
-        let mut update_number = true;
-        if push_button_red.pressed_transition() {
-            led_red.toggle();
-        } else if push_button_yel.pressed_transition() {
-            led_yel.toggle();
-        } else if push_button_grn.pressed_transition() {
-            led_grn.toggle();
-        } else if push_button_blu.pressed_transition() {
-            led_blu.toggle();
-        } else {
-            // no event
-            update_number = false;
+        let mut update_number = false;
+        for (i, pb) in &mut push_buttons.iter_mut().enumerate() {
+            if pb.pressed_transition() {
+                // We assume the same order within the arrays 'leds' and 'push_buttons'.
+                leds[i].toggle();
+                update_number = true;
+            }
         }
         if update_number {
-            let candles_lit =
-            led_red.state as u8 + led_yel.state as u8 + led_grn.state as u8 + led_blu.state as u8;
+            let mut candles_lit = 0;
+            for led in &mut leds {
+                candles_lit += led.state as u8;
+            }
             serial_println!("Candles lit: {}", candles_lit);
         }
         arduino_hal::delay_ms(10);
